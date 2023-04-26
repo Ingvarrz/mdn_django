581e28d63406fc24df14d8b8f6d0497d37ff1436
Ingvarrz <ikochim@gmail.com>
2023-04-26 15:11:29 +0300

    models tune admin page

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Book, Author, Genre, BookInstance
 
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+	fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'display_genre')
+
+
 admin.site.register(Genre)
 admin.site.register(BookInstance)
